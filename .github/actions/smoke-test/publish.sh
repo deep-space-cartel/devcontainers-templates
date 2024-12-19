8797f36a41077f363bf1f4e31ee5dc84e80a6fb5
Alexander Ilyin <alexander@ilyin.eu>
2024-12-19 10:43:45 +0000

    Allow to release from partcad branch

--- .github/actions/smoke-test/publish.sh
+++ .github/actions/smoke-test/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TEMPLATE_ID="$1"
 
+# TODO: @alexanderilyin run .github/workflows/release.sh
 devcontainer templates apply --template-id "ghcr.io/partcad/devcontainers-templates/${TEMPLATE_ID}" --workspace-folder "build/${TEMPLATE_ID}" --log-level trace
 # TODO: @alexanderilyin use "--label" option to add metadata to the image
 devcontainer build --push --log-level trace --image-name "ghcr.io/partcad/devcontainer-${TEMPLATE_ID}:latest" --workspace-folder "build/${TEMPLATE_ID}"
